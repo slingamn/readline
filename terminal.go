8b34e08d166b43434eab8183d77205bd097dc0a1
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2023-02-07 19:25:34 -0500

    placate `go vet`

--- terminal.go
+++ terminal.go
@@ -95,7 +95,7 @@ func (t *Terminal) Readline() *Operation {
 }
 
 // return rune(0) if meet EOF
-func (t *Terminal) ReadRune() rune {
+func (t *Terminal) GetRune() rune {
 	ch, ok := <-t.outchan
 	if !ok {
 		return rune(0)
